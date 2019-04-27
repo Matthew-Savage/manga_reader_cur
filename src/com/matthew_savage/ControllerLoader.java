989428ccc0c7c69328db3cb874c402dd031bc1d4
matthew-savage <matthew@matthew-savage.net>
2019-04-26 20:22:34 -0400

    cleanup

--- src/com/matthew_savage/ControllerLoader.java
+++ src/com/matthew_savage/ControllerLoader.java
@@ -34,7 +34,7 @@ public class ControllerLoader {
 
         if (InternetConnection.checkIfConnected()) {
             preloadProgressCenter.setText(Values.DIR_ROOT.getValue());
-//            executor.execute(this::fetchNewTitles);
+            executor.execute(this::fetchNewTitles);
 //            executor.execute(this::checkForUpdates);
         }
         executor.execute(this::launchMainApp);
@@ -60,76 +60,6 @@ public class ControllerLoader {
         preloadProgressCenter.setText("Checking For New Manga");
         populate.findStartingPage();
     }
-
-//    private void checkForUpdates() {
-//        clearProgressText();
-//        try {
-//            TimeUnit.MILLISECONDS.sleep(500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        preloadProgressCenter.setText("Checking for New Chapters");
-//        ArrayList<Integer> updateCheckAddresses = new ArrayList<>();
-//        int selectedManga;
-//        int startingChapter;
-//        String webAddress;
-//
-//        int updateTotalChapters;
-//        try {
-//            database.openDb(Values.DB_NAME_MANGA.getValue());
-//            ResultSet resultSet = database.filterManga("completed", "status", "Ongoing");
-//            if (resultSet.next()) {
-//                do {
-//                    updateCheckAddresses.add(resultSet.getInt("title_id"));
-//                } while (resultSet.next());
-//                resultSet.close();
-//                database.closeDb();
-//                for (int i = 0; i < updateCheckAddresses.size(); i++) {
-//                    selectedManga = updateCheckAddresses.get(i);
-//                    database.openDb(Values.DB_NAME_MANGA.getValue());
-//                    resultSet = database.filterManga("completed", "title_id", Integer.toString(selectedManga));
-//                    ArrayList chapterCount = IndexMangaChapters.getChapterAddresses(resultSet.getString("web_address"));
-//                    updateTotalChapters = resultSet.getInt("total_chapters");
-//                    webAddress = resultSet.getString("web_address");
-//                    startingChapter = (resultSet.getInt("last_chapter_read"));
-//                    database.closeDb();
-//                    System.out.println(chapterCount.size() + "  -  chapter count!");
-//                    if (chapterCount.size() > updateTotalChapters) {
-//                        int sizeDifference = chapterCount.size() - updateTotalChapters;
-//                        System.out.println("holy shit a manga " + updateCheckAddresses.get(i) + " has " + sizeDifference + " new chapters!");
-////                    chapterPages.getChapterPages(updateTotalChapters, updateWebAddress, Integer.toString(selectedManga));
-//                        database.openDb(Values.DB_NAME_MANGA.getValue());
-//                        database.modifyManga("completed", selectedManga, "total_chapters", chapterCount.size());
-////                    database.modifyManga("completed", selectedManga, "last_chapter_read", startingChapter); this doesnt change
-//                        database.modifyManga("completed", selectedManga, "new_chapters", 1);
-//                        database.closeDb();
-//                        //redundant with a method in ControllerMain class
-//                        database.openDb(Values.DB_NAME_DOWNLOADING.getValue());
-//                        database.openDb(Values.DB_NAME_MANGA.getValue());
-//                        database.downloadDbAttach();
-//                        database.moveManga(Values.DB_ATTACHED_PREFIX.getValue() + Values.DB_TABLE_COMPLETED.getValue(), Values.DB_ATTACHED_DOWNLOADING.getValue(), selectedManga);
-//                        database.deleteManga(Values.DB_ATTACHED_PREFIX.getValue() + Values.DB_TABLE_COMPLETED.getValue(), selectedManga);
-//                        database.downloadDbDetach();
-//                        database.closeDb();
-//                        database.closeDb();
-//                        //end redundant
-//
-////                        database.moveManga("completed", "download_pending", selectedManga);
-////                        database.closeDb();
-////                        dbMoveAndCopy.copyToDownloading(selectedManga, webAddress, startingChapter);
-//                    }
-//                }
-//            }
-//            clearProgressText();
-//            TimeUnit.MILLISECONDS.sleep(100);
-//            preloadProgressTop.setText("Update Check Complete");
-//            preloadProgressBottom.setText("Launching Reader");
-//            TimeUnit.MILLISECONDS.sleep(500);
-//        } catch (Exception e) {
-//            //Not Possible
-//            System.out.println(e);
-//        }
-//    }
 
     public void clearProgressText() {
         preloadProgressTop.clear();
--- src/com/matthew_savage/UpdateCollectedMangas.java
+++ src/com/matthew_savage/UpdateCollectedMangas.java
@@ -6,77 +6,9 @@ import java.util.concurrent.TimeUnit;
 
 public class UpdateCollectedMangas {
 
-    private ControllerMain controllerMain;
     private static Database database = new Database();
 
-
-    private static void checkForUpdates() {
-        try {
-            TimeUnit.MILLISECONDS.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        ArrayList<Integer> updateCheckAddresses = new ArrayList<>();
-        int selectedManga;
-        int startingChapter;
-        String webAddress;
-
-        int updateTotalChapters;
-        try {
-            database.openDb(Values.DB_NAME_MANGA.getValue());
-            ResultSet resultSet = database.filterManga("completed", "status", "Ongoing");
-            if (resultSet.next()) {
-                do {
-                    updateCheckAddresses.add(resultSet.getInt("title_id"));
-                } while (resultSet.next());
-                resultSet.close();
-                database.closeDb();
-                for (int i = 0; i < updateCheckAddresses.size(); i++) {
-                    selectedManga = updateCheckAddresses.get(i);
-                    database.openDb(Values.DB_NAME_MANGA.getValue());
-                    resultSet = database.filterManga("completed", "title_id", Integer.toString(selectedManga));
-                    ArrayList chapterCount = IndexMangaChapters.getChapterAddresses(resultSet.getString("web_address"));
-                    updateTotalChapters = resultSet.getInt("total_chapters");
-                    webAddress = resultSet.getString("web_address");
-                    startingChapter = (resultSet.getInt("last_chapter_read"));
-                    database.closeDb();
-                    System.out.println(chapterCount.size() + "  -  chapter count!");
-                    if (chapterCount.size() > updateTotalChapters) {
-                        int sizeDifference = chapterCount.size() - updateTotalChapters;
-                        System.out.println("holy shit a manga " + updateCheckAddresses.get(i) + " has " + sizeDifference + " new chapters!");
-//                    chapterPages.getChapterPages(updateTotalChapters, updateWebAddress, Integer.toString(selectedManga));
-//                        database.openDb(Values.DB_NAME_MANGA.getValue());
-//                        database.modifyManga("completed", selectedManga, "total_chapters", chapterCount.size());
-////                    database.modifyManga("completed", selectedManga, "last_chapter_read", startingChapter); this doesnt change
-//                        database.modifyManga("completed", selectedManga, "new_chapters", 1);
-//                        database.closeDb();
-//                        //redundant with a method in ControllerMain class
-//                        database.openDb(Values.DB_NAME_DOWNLOADING.getValue());
-//                        database.openDb(Values.DB_NAME_MANGA.getValue());
-//                        database.downloadDbAttach();
-//                        database.moveManga(Values.DB_ATTACHED_PREFIX.getValue() + Values.DB_TABLE_COMPLETED.getValue(), Values.DB_ATTACHED_DOWNLOADING.getValue(), selectedManga);
-//                        database.deleteManga(Values.DB_ATTACHED_PREFIX.getValue() + Values.DB_TABLE_COMPLETED.getValue(), selectedManga);
-//                        database.downloadDbDetach();
-//                        database.closeDb();
-//                        database.closeDb();
-                        //end redundant
-
-//                        database.moveManga("completed", "download_pending", selectedManga);
-//                        database.closeDb();
-//                        dbMoveAndCopy.copyToDownloading(selectedManga, webAddress, startingChapter);
-                    }
-                }
-            }
-            TimeUnit.MILLISECONDS.sleep(100);
-            TimeUnit.MILLISECONDS.sleep(500);
-        } catch (Exception e) {
-            //Not Possible
-            System.out.println(e);
-        }
-    }
-
     static void seeIfUpdated() {
-        System.out.println("update checker started!");
         ArrayList<MangaArrayList> checkList = fetchChecklist();
 
         for (MangaArrayList currentTitle : checkList) {
@@ -107,10 +39,7 @@ public class UpdateCollectedMangas {
         int newChapterCount = IndexMangaChapters.getChapterAddresses(webAddress).size();
 
         if (newChapterCount > currentTotalChapters) {
-            System.out.println(mangaId + " has new chapters!!");
             queueForDownload(mangaId, newChapterCount);
-        } else {
-            System.out.println(mangaId + " has NO new chapters.");
         }
     }
 
@@ -129,7 +58,6 @@ public class UpdateCollectedMangas {
                 checkList.add(new MangaArrayList(resultSet.getInt("title_id"), null, null, null, null, resultSet.getString("web_address"), null, resultSet.getInt("total_chapters"), 0, 0, 0, false, false));
             }
             resultSet.close();
-            System.out.println("checklist assembled, " + checkList.size() + " items big!");
         } catch (Exception e) {
             System.out.println(e);
         } return checkList;
