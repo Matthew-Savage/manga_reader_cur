181912d45c3eb88173c24b580d8c7299a97da87f
Matthew-Savage <matthew@matthew-savage.net>
2019-05-13 16:06:07 -0400

    converted from db filtering to stream filtering

---
+++ src/com/matthew_savage/GUI/ToggleNavButtons.java
@@ -0,0 +1,13 @@
+package com.matthew_savage.GUI;
+
+import java.util.ArrayList;
+
+public class ToggleNavButtons {
+
+    public ArrayList<Integer> navButtonVisibility(int pageNmber, int secondThingWhoKnows) {
+        ArrayList<Integer> values = new ArrayList<>();
+
+        return values;
+    }
+
+}
